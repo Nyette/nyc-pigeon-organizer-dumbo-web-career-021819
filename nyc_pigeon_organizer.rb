4f49ce2722d43c92d8f50cec0e1a2b0c5ef5faae
Antoinette Lee <antoinette.lee02@myhunter.cuny.edu>
2019-02-10 00:24:38 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,10 +3,24 @@ def nyc_pigeon_organizer(pigeon_data)
   pigeon_data.each do |attribute, details|
     details.each do |detail, name_array|
       name_array.each do |name|
-        pigeon_list[name] = {attribute => []}
-        # pigeon_list[name][attribute] << detail.to_s
+        pigeon_list[name] = {}
+      end
+    end
+  end
+  pigeon_data.each do |attribute, details|
+    details.each do |detail, name_array|
+      name_array.each do |name|
+        pigeon_list[name][attribute] = []
+      end
+    end
+  end
+  pigeon_data.each do |attribute, details|
+    details.each do |detail, name_array|
+      name_array.each do |name|
+        pigeon_list[name][attribute] << detail.to_s
       end
     end
   end
   pigeon_list
 end
+
